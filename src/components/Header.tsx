59ad5f4901e635015a1e8a135ed5e4c4d472f264
Nchang victory <97144931+vickyXuzhou@users.noreply.github.com>
2024-02-06 09:11:38 +0800

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './home/Home';
-import Pricing from './pricing/Pricing';
-import Team from './team/Team';
-import Contact from './contact/Contact';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./home/Home";
+import Pricing from "./pricing/Pricing";
+import Team from "./team/Team";
+import Contact from "./contact/Contact";
 
 function Header() {
   return (
@@ -11,13 +11,20 @@ function Header() {
       <div className="Header">
         <nav>
           <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/pricing">Pricing</Link></li>
-            <li><Link to="/team">Team</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/pricing">Pricing</Link>
+            </li>
+            <li>
+              <Link to="/team">Team</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
           </ul>
         </nav>
-
 
         <Routes>
           <Route path="/" element={<Home />} />
